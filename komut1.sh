1753615686e7edfff50d05f6e7acf9ca1bf63c15
Suphi ÇAKIR <suphicakir@gmail.com>
2020-02-08 22:18:41 +0300

    komut1.sh, komut2.sh dosyalarında değişiklikler yapıldı.

--- komut1.sh
+++ komut1.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-# komut1.sh
-echo "komut1.sh çalıştı."
-echo "Hello World"
+
--- komut2.sh
+++ komut2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # komut2.sh
 echo "komut2.sh çalıştı."
-echo "Merhaba dünya."
-read
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
